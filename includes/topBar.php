086a922bbdad2ea11ca3a800216acd6dbf7e06c7
Brian McCune <brian@bmccune.com>
2012-06-07 22:13:18 -0700

    New File

---
+++ includes/topBar.php
@@ -0,0 +1,19 @@
+<div class="row bmTop-Panel">
+					<div class="four columns hide-on-phones topbarblock">
+						Phone: 909-357-7300<br/>
+						Fax: 909-357-7304<br/>
+						<a href="mailto:office@prestigeanimalhospital.com">E-mail</a>
+					</div>
+					<div class="four columns hide-on-phones topbarblock">
+						10986 Sierra Ave.<br/>
+						Fontana, Ca 92337<br/>
+						<a href="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=10986+Sierra+Ave.+Fontana,+Ca+92337&amp;aq=&amp;sll=35.960223,-95.712891&amp;sspn=53.172343,102.65625&amp;ie=UTF8&amp;hq=&amp;hnear=10986+Sierra+Ave,+Fontana,+California+92337&amp;ll=34.054185,-117.435644&amp;spn=0.00648,0.012531&amp;t=m&amp;z=14" target="_blank">Map & Directions</a>
+					</div>
+					<div class="four columns hide-on-phones topbarblock">
+						<strong><h6>Hours:</h6></strong>
+						<strong>Mon, Tu, Fri:</strong> 8am-6pm<br/>
+						<strong>Wed and Thur:</strong> 8am-7pm<br/>
+						<strong>Sat:</strong> 9am-4pm<br/>
+						<strong>Sun:</strong> Closed
+					</div>
+				</div>
